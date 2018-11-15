f64c537393eadf33db534ee1e7fea5a0d9380289
vanyarin09 <ardrey09@gmail.com>
2018-11-15 18:13:41 +0800

    Added FindingASpecificWordInAString python script
    The program finds a specific word within the string

---
+++ Edx Python Scripts/FindingASpecificWordInAString.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 19:51:26 2018
+
+@author: Ardrey
+"""
+
+s = str(input("Enter a string: "))
+wordToFind = str(input("Enter the word you want to find: "))
+for c in range(len(s)):
+    if s[c:c+len(wordToFind)] == wordToFind:
+        print("The word: " + wordToFind + " is found within the string")
+        break
+    else:
+        print("There is no " + wordToFind + " found within the string")
+
