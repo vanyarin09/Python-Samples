5a0e91bccc27d9dc4ec0778b9b9fc8b754182a26
vanyarin09 <ardrey09@gmail.com>
2018-11-15 21:36:39 +0800

    Add files via upload
    Finding a square root using approximate solution

---
+++ Edx Python Scripts/FindingSquareRootUsingApproximateSolution.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 15 21:21:57 2018
+
+@author: Ardrey
+"""
+
+#Finding Square root using Approximate solution
+
+print("FINDING A SQUARE ROOT USING APPROXIMATE SOLUTION\n")
+square = int(input("Enter a number: "))
+epsilon = 0.01
+step = 0.001
+guess = 0.0
+
+while abs((guess**2 - square)) >= epsilon and guess <= square:
+    guess += step
+
+if abs(guess**2 - square) >= epsilon:
+    print ("The number " +str(square) +  " is not perfect square! ")
+else:
+    print ("The approximate square root of " + str(square) + " is " + str(guess))
+    
+
+
